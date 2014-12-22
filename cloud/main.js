c4f88f477b4decf3f5d06384f41737092d246fc2
David Alzate <davidalzate@gmail.com>
2014-12-22 10:41:32 -0500

    sendEmail

--- cloud/main.js
+++ cloud/main.js
@@ -3,4 +3,31 @@
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
+Mandrill.sendEmail({
+  message: {
+    text: "Hello World!",
+    subject: "Using Cloud Code and Mandrill is great!",
+    from_email: "parse@cloudcode.com",
+    from_name: "Cloud Code",
+    to: [
+      {
+        email: "you@parse.com",
+        name: "Your Name"
+      }
+    ]
+  },
+  async: true
+},{
+  success: function(httpResponse) {
+    console.log(httpResponse);
+    response.success("Email sent!");
+  },
+  error: function(httpResponse) {
+    console.error(httpResponse);
+    response.error("Uh oh, something went wrong");
+  }
+});
+
+
+  
 });
